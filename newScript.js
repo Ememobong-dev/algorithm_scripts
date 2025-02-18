0207c8f18b24dc44d6eacda0dea122282bcd8434
Olushola <ememobongumoren2@gmail.com>
2025-02-18 10:30:46 +0100

    update

--- newScript.js
+++ newScript.js
@@ -25,54 +25,43 @@
 // };
 
 const extractWord = (word) => {
-    let splittedArray = word.split("://");
-    // console.log(splittedArray, "Splitted");
-    if (splittedArray[0].includes("http")) {
-        splittedArray.splice(0,1);
-    }
-    console.log(splittedArray, "Spliced");
-    let otherKindOfLink = splittedArray[0].split(".");
-    // console.log(otherKindOfLink, "hiiii");
+  let baseName;
+  let domainName;
 
-    console.log(otherKindOfLink[-1].includes("/"))
+  let splittedArray = word.split("://");
 
-    // if(otherKindOfLink[-1].includes("/")){
-    //     otherKindOfLink[-1].split("/").splice(1, 1)
-    //     console.log(otherKindOfLink, "HOSTNAME");
-    // }
+  if (splittedArray[0].includes("http")) {
+    splittedArray.splice(0, 1);
+  }
 
+  let otherKindOfLink = splittedArray[0].split(".").slice(-2);
 
-    // let baseUrlArray;
-    // let otherKindOfLink = splittedArray[0].split(".");
-    // console.log(otherKindOfLink, "splitted");
-  
-    // if (splittedArray.length > 1) {
-  
-    //   if (splittedArray[0].includes("http")) {
-    //     const trial = splittedArray[2];
-    //     const splittedTrial = trial.split(".");
-    //     if (splittedTrial.length > 2) {
-    //       baseUrlArray = splittedTrial.slice(-2);
-    //       return baseUrlArray.join(".");
-    //     }
-    //     return splittedTrial.join(".");
-    //   } else {
-    //     // https weren't included in the link
-    //     return otherKindOfLink.join(".");
-    //   }
-    // } else {
-    //   // https weren't included in the link
-    //   return otherKindOfLink.join(".");
-    // }
-  };
+  baseName = otherKindOfLink[0]
 
+  if (otherKindOfLink[1].includes("/")) {
+    let xyy = otherKindOfLink[1].split("/");
+    xyy.splice(1);
+    domainName = xyy.join('')
+  } else {
+    domainName = otherKindOfLink[1];
+  }
+
+  return ` ${baseName}.${domainName}  `
+
+ 
+};
 
 console.log(extractWord("https://teamsdoc.io/app"));
 console.log(extractWord("https://google.com/jjjjjj")); //google.com
-console.log(extractWord("https://www.frontendmentor.io/challenges/character-counter-znSgeWs_i6")); // frontendmentor.io
-console.log(extractWord("https://www.wix.com/explore/websites/site/malco-wallshop"))
+console.log(
+  extractWord(
+    "https://www.frontendmentor.io/challenges/character-counter-znSgeWs_i6"
+  )
+); // frontendmentor.io
+console.log(
+  extractWord("https://www.wix.com/explore/websites/site/malco-wallshop")
+);
 console.log(extractWord("www.teamdocs.io/hello-there"));
-console.log(extractWord('teamdocs.io'));
-console.log(extractWord("https://www.teamdocs.io"))
-console.log(extractWord("https://api.www.teamdocs.io/abc"))
-
+console.log(extractWord("teamdocs.io"));
+console.log(extractWord("https://www.teamdocs.io"));
+console.log(extractWord("https://api.www.teamdocs.io/abc"));
